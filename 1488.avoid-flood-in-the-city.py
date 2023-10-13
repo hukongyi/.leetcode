f6f39ae29eaec07adf0a08a4282ae78d6211accc
hukongyi <372001980@qq.com>
2023-10-13 12:31:32 +0800

    1

--- 1488.avoid-flood-in-the-city.py
+++ 1488.avoid-flood-in-the-city.py
@@ -14,17 +14,26 @@ from typing import *
 
 
 class Solution:
+    # 定义一个避免洪水的函数，输入是一个整数列表，输出也是一个整数列表
     def avoidFlood(self, rains: List[int]) -> List[int]:
+        # 初始化答案列表，长度与输入列表相同，所有元素都为-1
         ans = [-1]*len(rains)
+        # 初始化一个字典，用于存储湖泊是否已满的信息
         full = dict()
+        # 初始化一个列表，用于存储可以抽水的天数
         pump = list()
+        # 遍历输入列表
         for i, num in enumerate(rains):
+            # 如果num为0，表示这一天没有下雨，可以抽水，将这一天添加到pump列表中
             if num == 0:
                 pump.append(i)
             else:
+                # 如果num在full的键中，表示这个湖泊已经满了，需要抽水
                 if num in full.keys():
+                    # 如果pump列表为空，表示没有可以抽水的天数，返回空列表
                     if len(pump) <= 0:
                         return []
+                    # 二分查找可以抽水的天数
                     left, right = 0, len(pump)
                     target = full[num]
                     while(left < right):
@@ -33,13 +42,18 @@ class Solution:
                             left = mid+1
                         else:
                             right = mid
+                    # 如果没有找到可以抽水的天数，返回空列表
                     if left == len(pump):
                         return []
                     else:
+                        # 否则，将找到的这一天从pump列表中移除，并在答案列表中标记这一天抽的是哪个湖泊的水
                         ans[pump.pop(left)] = num
+                # 更新湖泊的信息
                 full[num] = i
+        # 对于剩下的可以抽水的天数，随便抽哪个湖泊的水都可以
         for i in pump:
             ans[i] = 1
+        # 返回答案列表
         return ans
 
 
