f02315dbc0abc37fec9bc6fec0b8d064cd017e7a
Kongyi Hu <hukongyi@ihep.ac.cn>
2023-10-16 13:30:34 +0800

    daily

--- 260.single-number-iii.py
+++ 260.single-number-iii.py
@@ -11,7 +11,22 @@
 # @lcpr-template-end
 # @lc code=start
 class Solution:
+
+    def getxor(self, nums):
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
+
     def singleNumber(self, nums: List[int]) -> List[int]:
+        ans = self.getxor(nums)
+        for i in range(32):
+            if (ans >> i) & 1 == 1:
+                break
+        list1 = [num for num in nums if (num>>i)&1==1]
+        list2 = [num for num in nums if (num>>i)&1==0]
+        return [self.getxor(list1),self.getxor(list2)]
+        
 # @lc code=end
 
 
