80f0964083a2627faf10d871eeb976e706923267
hukongyi <372001980@qq.com>
2023-10-16 11:08:32 +0800

    1

--- 137.single-number-ii.py
+++ 137.single-number-ii.py
@@ -5,22 +5,29 @@
 # [137] 只出现一次的数字 II
 #
 
+
+# @lcpr-template-start
+
+# @lcpr-template-end
 # @lc code=start
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ans = 0
-        for i in range(32):
-            total = sum((num >> i) & 1 for num in nums)
-            if total % 3:
-                # Python 这里对于最高位需要特殊判断
-                if i == 31:
-                    ans -= (1 << i)
-                else:
-                    ans |= (1 << i)
+        nums_max = math.ceil(math.log2(max([abs(num) for num in nums])))
+        for i in range(nums_max+1):
+            total = sum([(abs(num) >> i) & 1 for num in nums])
+            if total % 3 != 0:
+                ans |= (1 << i)
+            # print(ans)
+            # print(bin(ans))
+        if min(nums) < 0:
+            i = 31
+            total = sum([(num >> i) & 1 for num in nums])
+            if total % 3 != 0:
+                ans = -ans
         return ans
 
 # @lc code=end
-
 
 
 #
@@ -32,5 +39,11 @@ class Solution:
 # [0,1,0,1,0,1,99]\n
 # @lcpr case=end
 
-#
+# @lcpr case=start
+# [-2,-2,1,1,4,1,4,4,-4,-2]\n
+# @lcpr case=end
 
+# @lcpr case=start
+# [-19,-46,-19,-46,-9,-9,-19,17,17,17,-13,-13,-9,-13,-46,-28]\n
+# @lcpr case=end
+#
---
+++ 260.single-number-iii.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=260 lang=python3
+# @lcpr version=30102
+#
+# [260] 只出现一次的数字 III
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,2,1,3,2,5]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [-1,0]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0,1]\n
+# @lcpr case=end
+
+#
+
