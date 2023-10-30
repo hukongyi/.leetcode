7d91638ae1c0f086c4adb2e0979639a7aedb18a8
Kongyi Hu <hukongyi@ihep.ac.cn>
2023-10-30 09:21:33 +0800

    update

---
+++ 2558.take-gifts-from-the-richest-pile.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=2558 lang=python3
+# @lcpr version=30102
+#
+# [2558] 从数量最多的堆取走礼物
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [25,64,9,4,100]\n4\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,1,1,1]\n4\n
+# @lcpr case=end
+
+#
+
