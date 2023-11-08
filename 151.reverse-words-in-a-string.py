a91af6e182c57790bfc29e6065703cc6fe3ae73a
hukongyi <372001980@qq.com>
2023-11-08 23:09:08 +0800

    update

--- 151.reverse-words-in-a-string.py
+++ 151.reverse-words-in-a-string.py
@@ -12,25 +12,10 @@
 # @lc code=start
 class Solution:
     def reverseWords(self, s: str) -> str:
-        slen = 0
-        s = list(s)
-        l = 0
-        r = 0
-        while(r < len(s)):
-            while(l < len(s) and s[l] == ' '):
-                l += 1
-                print(l)
-            r = l
-            while(r < len(s) and s[r] != ' '):
-                r += 1
-            print(l, r)
-            if r == len(s):
-                r += 1
-            s[slen:slen+r-l] = s[l:r:-1]
-            slen += r-l
-            l = r
-        return ''.join(s[:slen])
-
+        s = s.strip()
+        s = s[::-1]
+        s = ' '.join(word[::-1] for word in s.split())
+        return s
 # @lc code=end
 
 #
---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=28 lang=python3
+# @lcpr version=30105
+#
+# [28] 找出字符串中第一个匹配项的下标
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# "sadbutsad"\n"sad"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "leetcode"\n"leeto"\n
+# @lcpr case=end
+
+#
+
